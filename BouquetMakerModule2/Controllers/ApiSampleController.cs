1f55f063a60488dc43bb01af8dd3453fb4d75148
Anna Németh <anna.nemeth4@stud.uni-corvinus.hu>
2024-05-07 11:27:53 +0200

    Api kapcsolaton való munka

---
+++ BouquetMakerModule2/Controllers/ApiSampleController.cs
@@ -0,0 +1,29 @@
+﻿using Dnn.Bce.BouquetMakerModule2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dnn.Bce.BouquetMakerModule2.Controllers
+{
+    public class ApiSampleController : Controller
+    {
+        // GET: ApiSample
+        public ActionResult Index()
+        {
+            // Default URL and API key
+            var url = "http://20.234.113.211:8090/";
+            var key = "1-16278226-9374-49b4-81a8-5a5bd6adfec0";
+
+            // Pass URL and key to the view
+            var viewModel = new ApiSampleViewModel
+            {
+                Url = url,
+                Key = key
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ BouquetMakerModule2/RouteMapper.cs
@@ -0,0 +1,23 @@
+﻿using DotNetNuke.Web.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Dnn.Bce.BouquetMakerModule2
+{
+    public class RouteMapper //: IMvcRouteMapper
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                name: "BouquetMakerModule2",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Dnn.Bce.BouquetMakerModule2.Controllers" }
+            );
+        }
+    }
+}
